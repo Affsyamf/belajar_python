113e1cb20b4673e4ee5a97cf27e2d80b5925f790
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-03 17:17:47 +0700

    mengubah data pada python

---
+++ 1.py
@@ -0,0 +1,11 @@
+a = 10
+b = 15
+c = a + b
+d = False
+print("Nilai B =", d, "Tipe datanya :", type(d))
+
+from ctypes import c_double
+
+e = c_double(3.14)
+print("Nilai E adalah : ", e)
+print("Tipe datanya adalah : ", type(e)) 
---
+++ rubahdata.py
@@ -0,0 +1,21 @@
+# a = 10
+# datafloat = float(a)
+# print("Nilai dari A adalah :", a, "tipe datanya :", type(a))
+# print("Ubah data a ke float menjadi : ", datafloat, "Tipe datanya : ", type(datafloat))
+
+# b = 5
+# datastring = str(b)
+# databool = bool(b)
+# print("Data B : ", b, "Tipe : ", type(b))
+# print("Data B menjadi :", datastring, "Tipe : ", type(datastring))
+# print("Nilai Boolean : ", databool, "Tipe - ", type(databool))
+
+# float to int
+c = 5.3
+datastring = str(c)
+databool = bool(c)
+dataint = int(c)
+print("Data B : ", c, "Tipe : ", type(c))
+print("Data B : ", dataint, ", Tipe -", type(dataint))
+print("Data B menjadi :", datastring, "Tipe : ", type(datastring))
+print("Nilai Boolean : ", databool, "Tipe - ", type(databool))
