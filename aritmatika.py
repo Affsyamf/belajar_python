89fb3f8ea77276fc08a8d24354d29378b54b1744
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-03 20:19:08 +0700

    aritmatika

---
+++ aritmatika.py
@@ -0,0 +1,10 @@
+a  = 10
+b = 5
+
+print("Penjumlahan : ", a + b)
+print("Pengurangan : ", a - b)
+print("Perkalian : ", a * b)
+print ("Pembagian : ", a / b)
+print ("Modulus : ", a % b)
+print ("Eksponen : ", a ** b)
+print ("Floor Division : ", a // b)
