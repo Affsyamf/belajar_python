c8a28190bd7ef44d8dffde95878caab9deae5bb8
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-04 16:53:27 +0700

    latihan 2

---
+++ latihan2.py
@@ -0,0 +1,48 @@
+# gabungan rentang angka
+
+# masukan angka kurang dari 3 dan lebih dari 10
+
+# user = float(input("Masukan Angka : "))
+
+# # cek angka kurang dari 3
+# iskurang = (user < 10)
+# print ("Angka : ", user,  "\nHasil : ", iskurang)
+# islebih = (user >= 3) 
+# print ("Hasil : ", islebih)
+
+# # OR
+# hasil = iskurang or islebih
+# print("Hasil : ", hasil)
+
+# # and
+# hasil = iskurang and islebih
+# print("Hasil : ", hasil)
+
+# # xor
+# hasil = iskurang ^ islebih
+# print("Hasil : ", hasil)
+
+
+# LATIHAN
+
+print("======= LATIHAN =======")
+
+user = float(input("Masukan Angka :"))
+
+# lebih dari 0
+k1 =  0 < user
+print("Angka  : ", user, "\nHasil : ", k1 )
+
+# kurang dari 5
+k2 = user < 5
+print("Hasil :", k2)
+
+# kurang dari 8
+k3 = user > 8
+print("Hasil : ", k3)
+
+# kurang dari 11
+k4 = user < 11
+print("Hasil  : ", k4)
+
+
