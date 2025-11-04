bb12f0d3cbed4b4a9944ad0185c59b8884a44b68
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-04 20:31:19 +0700

    assignment

---
+++ assignment.py
@@ -0,0 +1,29 @@
+a = 2
+print("Hasil : ", a)
+
+a += 1
+print("Hasil : ", a)
+
+a += 1
+print("Hasil : ", a)
+
+a -= 2
+print("Hasil : ", a)
+
+a *= 2 
+print("Hasil kali : ", a)
+
+a /= 2
+print("Hasil : ",a)
+
+b = 10
+print("B : ", b)
+
+b %= 3
+print("Hasil : ", b)
+
+b // 2
+print("Hasil : ", b)
+
+b ** 2
+print("Hasil : ",b)
