5202b1c3da61376064e3a92798370d08294a077e
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-05 06:55:26 +0700

    belajar format string

---
+++ format.py
@@ -0,0 +1,20 @@
+nama = "afif"
+format_str = f"hallo {nama} "
+print(format_str)
+
+angka = 110
+format = f"Ini angka : {angka}"
+print(format)
+angka = 119.101
+format = f"Desimal : {angka:.2f}"
+print(format)
+
+
+harga = 1000000
+angka = 5
+jumlah = f"Total : {harga*angka:,}"
+print(jumlah)
+
+
+# makan = str(input("Makanan : "))
+# print(makan)
