310c9d15132cd08c691694c18da3399106c8421a
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-04 20:59:09 +0700

    string

---
+++ string.py
@@ -0,0 +1,17 @@
+nama = "Afif Syam Fauzi"
+print(nama)
+
+print("C:\new folder")
+print(r"C:\new folder")
+
+# multi literal
+
+print(r""" 
+Nama : Afif Syam Fauzi
+NIM  : 6702223011
+      """)
+
+print(""" 
+Nama : \nAfif Syam Fauzi
+NIM  : 6702223011
+      """)
