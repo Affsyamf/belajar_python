abc0fe528a1ed52576f09b3456d0bb97e629fb64
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-04 12:12:04 +0700

    komparasi aritmatika

---
+++ komparasi.py
@@ -0,0 +1,13 @@
+a = 5
+b = 2
+d = 5
+e = 3
+c = a >= 5
+print("Hasil : ", c, "Tipe : ", type(c))
+print("Hasil : ", a == 5)
+print("Hasil : ", b == 1)
+print("Hasil : ", a != 4)
+print("Hasil : ", b != 2)
+print("Hasil  : ", d is e)
+print("Hasil  : ", d is not e)
+
