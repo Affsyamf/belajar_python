ce71b4414de460ad9e8b7265e0861d1a333f1d7b
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-05 03:59:55 +0700

    operasi2

---
+++ operasi2.py
@@ -0,0 +1,25 @@
+# operator dalam method
+
+salam = "Halo"
+print(f"Afif {salam}")
+print(f"afif {salam}".capitalize())
+print(salam.capitalize())
+print(salam.upper())
+print(salam.lower())
+print("Apakah kecil : " + str(salam.islower()))
+print("Apakah Upper : " + str(salam.isupper()))
+
+nama = "Afif Syam Fauzi"
+print("Cek : " + str(nama.isalpha()))
+print("Cek : " + str(nama.isascii()))
+print("Cek : " + str(nama.isdecimal()))
+print("Cek : " + str(nama.isalnum()))
+
+nama = ["Afif","Syam","Fauzi"]
+print(nama)
+print(", ".join(nama))
+print(" ".join(nama))
+
+nama = "afif1syam1fauzi"
+print(nama)
+print(nama.split("1"))
