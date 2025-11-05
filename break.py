44b384a4c7a6e3c740bc94d11c593dec2082f7e4
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-06 04:00:02 +0700

    break

---
+++ break.py
@@ -0,0 +1,16 @@
+# break
+
+angka = 0
+user = int(input("Masukan Angka : "))
+
+while True:
+    angka += 1
+    print(f"angka skrg : {angka}")
+    
+    if angka == user:
+        print("Skrg Angka " , angka)
+        break
+        
+    print("Afif")
+    
+print("end")
