6450b8c5106127b9c381a782bf169119e5dfd962
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-06 03:31:12 +0700

    while

---
+++ while.py
@@ -0,0 +1,14 @@
+# while loop
+
+# kata = ["afif", "misel"]
+# for i in kata:
+#     print(i)
+    
+    
+    
+angka = 0
+print(f"Angka : {angka}")
+while angka < 5:
+    angka += 1
+    print(f"Angka : {angka}")
+    print(f"afif")
