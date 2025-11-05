98a7179fa4157b739c8893c8b4b879aecc588f38
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-05 23:41:55 +0700

    looping

---
+++ looping.py
@@ -0,0 +1,36 @@
+# perulangan
+# for konddisi:
+#     aksi
+
+# ini memakai list
+angka = [1, 2, 3, 4, 5]
+print(angka)
+
+for i in angka:
+    print(f"i = {i}")
+
+print("\nbatas\n")
+
+# ini memakai range
+angka1 = range(5)
+
+for i in angka1:
+    print(f"i - {i}")
+    
+    
+print("\nbatas\n")
+
+# ini memakai range
+angka1 = range(0,5)
+
+for i in angka1:
+    print(f"i - {i}")
+    
+print("\nBatas\n")    # 
+
+# menggunakan str
+data = ["afif", "misel", "mahira"]
+
+for kata in data:
+    print(f"halo {kata}")
+    
