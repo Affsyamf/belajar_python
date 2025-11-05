157a98ee65ad279e0d1bed3bb6fbadedecf9b20f
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-05 19:25:17 +0700

    percabangan

---
+++ ifelse.py
@@ -0,0 +1,14 @@
+nama = input("Nama : ")
+
+# if nama=="afif" : print("Ganteng")
+# print(f"Terimakasih : {nama}")
+
+if nama=="afif":
+    print("kamu gg")
+    print('ggwp')
+print(f"Terimakasih : {nama}")
+
+if nama=="misel":
+    print("Cantik")
+else:
+    print("Kamu bukan misel")
