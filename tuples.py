c9e35d3a39df016239ff26ed9fbc98ea181a81f9
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-06 23:52:06 +0700

    tuples dan sets

---
+++ tuples.py
@@ -0,0 +1,14 @@
+
+# tuples
+data1 = [1,2,3,4]
+data1[1] = "afif"
+data1.append(1)
+print(data1)
+
+data2 = (1,2,3,4)
+print(data2[1])
+
+# sets auto sort
+sets = {10,6,3,7,8,3,300,1,'z','a',9,2,1,5}
+sets.add(100)
+print(sets)
