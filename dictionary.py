e5e66dce1dffaa99576b62643c4f7c45a401897e
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-07 00:01:56 +0700

    dictionary untuk membuat identifier -> key

---
+++ dictionary.py
@@ -0,0 +1,11 @@
+nama = ["afif", "misel", "chimi"]
+print(f"Daftar Nama : \n{nama}")
+
+data = {
+    'key':'value',
+    'a'  : 'afif',
+    'm'  : 'misel',
+    'nama': nama
+}
+
+print(data['nama'])
