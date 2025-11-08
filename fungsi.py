987e52af575e6f417af43ef9ebdce3a2ece0adf4
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-08 17:01:38 +0700

    belajar menggunakan fungsi, parameter, list

--- fungsi.py
+++ fungsi.py
@@ -1,4 +1,59 @@
-def afif():
-    print(f"Afif Syam fauzi \n6702223011 \nBekasi")
+def nama():
+    print(f"""
+Afif Syam Fauzi
+6702223011
+TK
+          """)
+nama()
+
+
+def hallo (nama):
+    print(f"Halo {nama}")
     
-afif()
+hallo("Afif")
+hallo("misel")
+    
+
+# hitung
+def hitung (angka1, angka2):
+    hasil = angka1 + angka2
+    print(f"{angka1} + {angka2} = {hasil}")
+    
+hitung(1,2)
+
+
+# list
+def anggota (peserta):
+    data_peserta = peserta.copy()
+    for band in data_peserta:
+        print(f"Nama Anggota : {band}")
+
+anggota_band = ["Afif", "opid", "aril"]
+
+anggota(anggota_band)
+
+
+# menggunakan join
+def anggota(band):
+    semua = ", ".join(band)
+    print(f"Anggota Band : {semua}")
+    
+anggota_band = ["afif", "misel"]  
+anggota(anggota_band)
+ 
+ 
+#  menggunakan sederhana
+def anggota(band):
+    print(f"Daftar : {band}")
+    
+anggota_band = ["chimi", "amira"]
+anggota(anggota_band)
+
+# memnaggil satu per satu terpisah
+def anggota (band):
+    print(f"Anggota 1 : {band[0]}")
+    print(f"Anggota 2 : {band[1]}")
+    
+anggota_band = ["opid", "ayul"]
+anggota(anggota_band)
+
