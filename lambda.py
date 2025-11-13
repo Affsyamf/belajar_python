7f6bf2de5d60d03f2658a4ff75d0e8cf215d8b56
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-13 11:52:20 +0700

    lambda function

--- lambda.py
+++ lambda.py
@@ -24,10 +24,42 @@ data_list.sort()
 
 # sorting list angka
 data_angka = [1,3,4,2,5]
-print(f"Data Angka : {data_angka}")
 data_angka.sort()
+print(f"Data Angka : {data_angka}")
 
 # sorting tupel angka
 data_angka2 = (1,10,4,5,2,3,8,6,9,7)
 print(f"Data Angka Tupel : {sorted(data_angka2)}")
 
+
+data_nama = ["Afif", "Risna", "Louise"]
+data_nama.sort()
+print(f"Sorted List By Lambda : {data_nama}")
+
+# kasus ganji
+angka_baru = [1,2,3,4,5,6,7,8]
+
+def data_angka(angka):
+    return angka < 5
+data_angka_baru = list(filter(data_angka, angka_baru))
+print(f"Hasil angka baru : {data_angka_baru}")
+
+# kasus genap
+data_genap = list(filter(lambda x:(x%2==0), angka_baru))
+print(data_genap)
+
+# kasus ganjil 
+data_ganjil = list(filter(lambda x:(x%2==1), angka_baru))
+print(data_ganjil)
+
+# anonymouse function
+def pangkat(angka,n):
+    return angka**n
+print(pangkat(3,2))
+
+def pangkat(n):
+    return lambda angka:angka**n
+
+pangkat2 = pangkat(3)
+print(f"Hasil Pangkat : {pangkat2(3)}")
+print(f"Pangkat : {pangkat(4)(3)}")
