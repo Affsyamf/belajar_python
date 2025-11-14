a99493251a118fc8343d5761913fe1f68c9a994f
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-14 17:19:43 +0700

    penjelasan __init__.py Package pada python

---
+++ main_app.py
@@ -0,0 +1,13 @@
+import sains
+
+hasil_tambah = sains.matematika.tambah(1,2,3)
+print(f"Hasil tambah : {hasil_tambah}")
+
+hasil_kali = sains.matematika.kali(2,2)
+print(f"Hasil kali : {hasil_kali}")
+
+hasil_fisika = sains.fisika.gaya(10,10)
+print(f"Hasil fisika : {hasil_fisika}")
+
+hasil_pangkat = sains.matematika.pangkat(3)
+print(f"Hasil Pangkat : {hasil_pangkat(5)}")
--- sains/matematika/basic.py
+++ sains/matematika/basic.py
@@ -1,5 +1,3 @@
-'''Module matematika'''
-
 def tambah(*args):
     hasil = 0
     for data in args:
@@ -10,9 +8,6 @@ def tambah(*args):
 def kali(*args):
     hasil = 1
     for data in args:
-        hasil *+ data
+        hasil *= data
         
     return hasil
-    
-def pangkat(n):
-    return lambda angka:angka**n
