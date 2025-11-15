f3e6d5f43e7491b0be5a705a3f5bbef45c5c3e0c
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-15 14:54:52 +0700

    pygame

---
+++ 62_pygame.py
@@ -0,0 +1,55 @@
+import pygame
+
+# init
+pygame.init()
+# variabel running game
+isrun = True
+
+# membuat display surface object
+window_lebar = 500
+window_panjang = 500
+window = pygame.display.set_mode((window_lebar,window_panjang))
+
+# object game
+# posisi
+x = 250
+y = 250
+
+# ukuran
+panjang = 20
+lebar = 20
+
+# kecepatan gerak
+speed = 10
+
+while isrun:
+    pygame.time.delay(10)
+    # user input, database input
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+           isrun = False
+           
+    # ambil semua keyboard press
+    keys = pygame.key.get_pressed()
+    
+    # ambil ke kiri
+    if keys[pygame.K_LEFT] and x > 0:
+        x -= speed
+        
+    if keys[pygame.K_RIGHT] and x < window_lebar-lebar:
+        x += speed
+    
+    if keys[pygame.K_DOWN] and y < window_panjang-panjang:
+        y += speed
+        
+    if keys[pygame.K_UP] and y > 0:
+        y -= speed
+           
+    # update asset
+    window.fill((255,255,255))
+    pygame.draw.rect(window, (255,150,0),(x,y,lebar,panjang))
+    # render ke display
+    pygame.display.update()
+        
+pygame.quit()
+
