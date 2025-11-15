508a93cb9d986d0c76fa54fed416941df7d1bb42
Afif Syam Fauzi <afiffauzi2016@gmail.com>
2025-11-15 13:39:38 +0700

    61 package numpy

---
+++ 61_numpy.py
@@ -0,0 +1,14 @@
+import numpy as np
+
+list_a = [1,2,3,4]
+vector_a = np.array([1,2,3,4])
+
+print(f"List A : {list_a}")
+# print(f"List A Kuadrat : {list_a**2}")
+
+print(f"Vector A : {vector_a}")
+print(f"Vector Kuadrat : {vector_a**2}")
+
+matrix_b = np.array([(1,2),(3,4)])
+print(f"Matrix B : \n{matrix_b}")
+print(f"Matrix B : \n{matrix_b**2}")
